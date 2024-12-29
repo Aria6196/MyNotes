d3bb857451db6f2585bf1d8db170a81aca9252d4
DikaHaekalFirzaPratama <dikahaekalfirza@gmail.com>
2024-12-29 21:42:50 +0700

    Update Memperbaiki Navbar

--- app/src/main/java/proyek/pam/mynotes/MainActivity.kt
+++ app/src/main/java/proyek/pam/mynotes/MainActivity.kt
@@ -1,6 +1,8 @@
 package proyek.pam.mynotes
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,7 @@ import proyek.pam.mynotes.database.NoteDatabase
 import proyek.pam.mynotes.repository.NoteRepository
 import proyek.pam.mynotes.viewmodel.NoteViewModel
 import proyek.pam.mynotes.viewmodel.NoteViewModelFactory
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,12 +22,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Menambahkan Toolbar ke ActionBar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Setup ViewModel
         setupViewModel()
     }
 
-    private fun setupViewModel(){
+    private fun setupViewModel() {
         val noteRepository = NoteRepository(NoteDatabase(this))
         val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
         noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+
+    // Inflasi menu dan menampilkan SearchView
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    // Menangani klik item menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.searchMenu -> {
+                // Logika pencarian bisa ditambahkan di sini
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
